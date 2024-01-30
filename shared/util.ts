ca545972b0cdcbc9472e31a3e2067bf807c51eeb
Shijin Zhang <1364226740@qq.com>
2024-01-30 08:18:03 +0800

    Lambda communicating with DynamoDB

--- shared/util.ts
+++ shared/util.ts
@@ -2,16 +2,9 @@ import { marshall } from "@aws-sdk/util-dynamodb";
 import { Movie } from "./types";
 
 export const generateMovieItem = (movie: Movie) => {
-    const item = {
-        movieId: movie.id,
-        ...movie,
-        id: undefined
-    };
-    delete item.id;
-
     return {
         PutRequest: {
-            Item: marshall(item),
+            Item: marshall(movie),
         },
     };
 };
